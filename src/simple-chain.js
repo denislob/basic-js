43647b07ce6b60a9bda6d3be4b945df998faaff7
den <you@example.com>
2025-11-10 00:21:33 +0300

    feat: add a solution to the task - simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,34 @@ const { NotImplementedError } = require('../lib');
  *
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    if (value === undefined) {
+      this.chain.push('( )');
+    } else {
+      this.chain.push(`( ${value} )`);
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    if (!Number.isInteger(position) || position < 1 || position > this.chain.length) {
+      this.chain = [];
+      throw Error("You can't remove incorrect link!")
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    const finalChain = this.chain.join('~~');
+    this.chain = [];
+    return finalChain;
   },
 };
 
